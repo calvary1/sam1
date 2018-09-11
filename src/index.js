4ad77549297326297c3d60f99f468cd7ab2180ec
Jessica <evasoserious@gmail.com>
2018-09-11 11:11:47 +0200

    Giraffe

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("test ok")
+var tabletop = require('tabletop')
+var render = require('./render')
+
+
+
+
+
+tabletop.init({
+    key: 'https://docs.google.com/spreadsheets/d/1LBDm0k2Ocn5HwgbeIV_2ZEegLw6Gidtns3Yz_N-f7_A/pubhtml',
+    callback: function (data, tabletop) {
+        if( render = kordofan)
+        console.log('tabletop data:', kordofan)
+        var view = render(kordofan)
+        document.body.appendChild(kordofan)
+    },
+    simpleSheet: true
+   })
---
+++ src/render.js
@@ -0,0 +1,16 @@
+var crel = require('crel')
+
+function renderApp(data) {
+    document.getElementById("div")
+    <div; id = "kordofan">
+   for (var i = 0; i = i +1) {
+    ( crel = giraffe)
+
+   }
+
+    return crel('h1', 'kordofan')
+}
+
+console.log(kordofan)
+module.exports = renderApp
+
